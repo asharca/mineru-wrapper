9f199c2ba225d32b2077c6669df78de48dc78771
asharca <129170100+asharca@users.noreply.github.com>
2026-06-08 18:51:42 +0800

    chore: 应用 biome 自动排序的 import

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/contexts/ThemeContext";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { SettingsProvider } from "./SettingsContext.tsx";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 import App from "./App.tsx";
+import { SettingsProvider } from "./SettingsContext.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
